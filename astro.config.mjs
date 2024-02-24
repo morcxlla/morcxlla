88d9739b80be5a8d19ec9365c2d053a37ea86523
Morcilla_ <jorgecividanes2016@gmail.com>
2024-02-24 13:40:34 +0100

    Tailwind + Onest Font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
+// Supports weights 100-900
+import '@fontsource-variable/onest';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()]
+});
