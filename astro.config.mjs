f154ec0279aefdfe2f86ad54af8de6cf55ff9e66
Morcilla_ <jorgecividanes2016@gmail.com>
2024-03-09 14:20:05 +0100

    Redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,13 @@
 import { defineConfig } from 'astro/config';
-import { social } from '../data/config.js'
+import { redirects } from '../data/config.js'
 
 import tailwind from "@astrojs/tailwind";
 
 // Supports weights 100-900
 import '@fontsource-variable/onest';
 
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  redirects: {
-    '/ai-album'{
-      to: 'http://www.ai-album.art',
-      target: '_blank'
-    }
-
-  }
+  redirects: redirects
 });
-
-
--- src/data/config.js
+++ src/data/config.js
@@ -13,3 +13,9 @@ export const theme = {
   h2: 'text-3xl font-extrabold',
   h3: 'text-2xl font-bold',
 }
+
+export const redirects = {  
+  '/ai-album': {
+    to: 'http://www.ai-album.art',
+  },
+}
